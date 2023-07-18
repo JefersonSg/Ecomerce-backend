a2d1ffa54e6d12e10af9f81baab37f7fb1b33379
JefersonSg <jefersongabri@gmail.com>
2023-07-18 08:14:58 -0300

    git ignor

--- db/conn.js
+++ db/conn.js
@@ -1,5 +1,7 @@
 const mongoose = require('mongoose');
 
+// const url = "mongodb+srv://jefersongabri:<password>@lojamayse.rb3z4d8.mongodb.net/?retryWrites=true&w=majority";
+
 async function main() {
   await mongoose.connect(
     'mongodb+srv://jefersongabri:Izn1lKLZ0FhI0ys7@lojamayse.rb3z4d8.mongodb.net/',
